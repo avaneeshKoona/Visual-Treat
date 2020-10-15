d3983b1068cb665e55c5abb61074fd0d300dd53b
avaneesh19 <kavaneesh19@gmail.com>
2020-10-15 15:23:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.render("home");
+  })
+
+  app.listen(3000, function() {
+    console.log("The server is running on port 3000");
+  })
+  
+
